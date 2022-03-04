c0b388ce16369e2f756a55681740022c3c358397
walelgn <walelgnlinux@gmail.com>
2022-03-04 18:59:05 +0300

    theme color created

---
+++ lib/utils/colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ColorProvider{
+  var primaryColor = Colors.yellow;
+  var primaryDarkColor = Colors.amber;
+
+  var secondaryColor = Colors.orange;
+  var secondaryDarkColor = Colors.deepOrange;
+
+  var primaryTextColor = Colors.black54;
+  var primaryDarkTextColor = Colors.black;
+
+  var secondaryTextColor = Colors.white;
+  var secondaryDarkTextColor = Colors.grey;
+
+  var errorColor = Colors.red;
+  var successColor = Colors.green;
+
+  var blue = Colors.blue;
+  var green = Colors.green;
+  var yellow = Colors.yellow;
+  var orange = Colors.orange;
+  var white = Colors.white;
+}
