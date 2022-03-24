04382f0c161c2bf9aa9b5ac1708366ab37ccfe44
walelgn <walelgnlinux@gmail.com>
2022-03-24 15:29:25 +0300

    feed back updated

--- lib/dataprovider/send_feedback.dart
+++ lib/dataprovider/send_feedback.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 import 'package:passengerapp/dataprovider/dataproviders.dart';
 import 'package:passengerapp/models/models.dart';
@@ -9,18 +10,20 @@ class SendFeedback {
   AuthDataProvider dp = AuthDataProvider(httpClient: http.Client());
   final _baseUrl = 'https://safeway-api.herokuapp.com/api/locations';
   final http.Client httpClient;
-
+  final secure_storage = const FlutterSecureStorage();
   SendFeedback({required this.httpClient});
-  Future<String> sendFeedback(String senderPhone,message) async {
+  Future<String> sendFeedback(message,description) async {
+    var user_phone = secure_storage.read(key: "phone_number");
     final http.Response response = await httpClient.post(
-        Uri.parse('$_baseUrl/send-feedback'),
+        Uri.parse('$_baseUrl/feedbacks/create-feedback'),
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
         },
         body: json.encode({
           'driver_id': 'waiting',
-          'sender_phone': senderPhone,
-          'message': message,
+          'sender_phone': user_phone,
+          'subject': message,
+          'description': description,
         }));
     if(response.statusCode == 200){
       return response.body;
